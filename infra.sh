a00c10b1b9b0161d038fbfb7f17fef113cd6e130
Artur Pragacz <artur@pragacz.com>
2023-01-24 21:15:15 +0100

    Fix jinja templates.

--- infra.sh
+++ infra.sh
@@ -5,11 +5,10 @@ gcloud builds submit --config build.yaml .
 
 count=3
 echo "Generating kubernetes resources from templates..."
-for n in {1..$count}
-do
-  jinja2 node/deployment.tmpl -D project_id=$project_id -D number=$n -D count=$count > k8s/node-$n.yaml
+for i in $(seq 1 $count); do
+  jinja2 node/deployment.tmpl -D project_id=$project_id -D number=$i -D count=$count > k8s/node-$i.yaml
 done
-jinja2 probler/prober.tmpl -D project_id=$project_id -D number=$n -D count=$count > k8s/prober.yaml
+jinja2 prober/deployment.tmpl -D project_id=$project_id -D number=$i -D count=$count > k8s/prober.yaml
 
 echo "Creating K8s resources..."
 gcloud container clusters create-auto banking-cluster --region europe-central2
